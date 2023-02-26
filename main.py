41a0b93a08832575986a9631a4699154a93c84de
qwerty <ungs_07@mail.ru>
2023-02-26 12:27:56 +0300

    change2

--- main.py
+++ main.py
@@ -4514,42 +4514,43 @@ import math
 # print(weight.kg, "кг => ", end="")
 # print(weight.to_pounds(), "фунтов")
 
-
-class Person:
-    def __init__(self, name, old):
-        self.__name = name
-        self.__old = old
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, n):
-        self.__name = n
-
-    @name.deleter
-    def name(self):
-        del self.__name
-
-    @property
-    def old(self):
-        return self.__old
-
-    @old.setter
-    def old(self, new_old):
-        self.__old = new_old
-
-    @old.deleter
-    def old(self):
-        del self.__old
-
-
-p1 = Person("Irina", 26)
-print(p1.__dict__)
-p1.name = "Igor"
-print(p1.name)
-p1.old = 31
-print(p1.old)
-del p1.old
-print(p1.__dict__)
+#
+# class Person:
+#     def __init__(self, name, old):
+#         self.__name = name
+#         self.__old = old
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, n):
+#         self.__name = n
+#
+#     @name.deleter
+#     def name(self):
+#         del self.__name
+#
+#     @property
+#     def old(self):
+#         return self.__old
+#
+#     @old.setter
+#     def old(self, new_old):
+#         self.__old = new_old
+#
+#     @old.deleter
+#     def old(self):
+#         del self.__old
+#
+#
+# p1 = Person("Irina", 26)
+# print(p1.__dict__)
+# p1.name = "Igor"
+# print(p1.name)
+# p1.old = 31
+# print(p1.old)
+# del p1.old
+# print(p1.__dict__)
+print("hello")
